b52fc8e712aa8aacf6bfb30ea95ac7c65150422f
Doug Boyd <doug@dougboyd.com.au>
2021-12-18 08:39:34 +1100

    Slimmed down version, 1

--- projects/fotolicious/src/app/app-routing.module.ts
+++ projects/fotolicious/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'about',
+    redirectTo: 'fotolicious',
     pathMatch: 'full'
   },
   {
@@ -15,34 +15,8 @@ const routes: Routes = [
       )
   },
   {
-    path: 'about',
-    loadChildren: () =>
-      import('./features/about/about.module').then((m) => m.AboutModule)
-  },
-  {
-    path: 'feature-list',
-    loadChildren: () =>
-      import('./features/feature-list/feature-list.module').then(
-        (m) => m.FeatureListModule
-      )
-  },
-  {
-    path: 'settings',
-    loadChildren: () =>
-      import('./features/settings/settings.module').then(
-        (m) => m.SettingsModule
-      )
-  },
-  {
-    path: 'examples',
-    loadChildren: () =>
-      import('./features/examples/examples.module').then(
-        (m) => m.ExamplesModule
-      )
-  },
-  {
     path: '**',
-    redirectTo: 'about'
+    redirectTo: 'fotolicious'
   }
 ];
 
